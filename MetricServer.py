53993deca1cde02cae8bc973d7cda72934d7568b
Farkhat Yakubov <farhatyakubov@gmail.com>
2020-10-06 23:44:33 +0600

    Added example client code; Added process_data method;

--- MetricServer.py
+++ MetricServer.py
@@ -1,28 +1,57 @@
 import asyncio
 
 
+class SeverError(Exception):
+    """Server Exception Class"""
+    pass
+
+
 class ClientServerProtocol(asyncio.Protocol):
+    def __init__(self):
+        self.transport = None
+
     def connection_made(self, transport):
         self.transport = transport
 
     def data_received(self, data):
-        resp = process_data(data.decode())
+        resp = self.process_data(data.decode())
         self.transport.write(resp.encode())
 
+    def process_data(self, data):
+        print(data)
+        command, payload = data.split(maxsplit=1)
 
-loop = asyncio.get_event_loop()
-coro = loop.create_server(
-    ClientServerProtocol,
-    '127.0.0.1', 8181
-)
+        if command == 'put':
+            key, value, timestamp = payload.split()
+            response = self.put(key, value, timestamp)
+        elif command == 'get':
+            response = self.get(payload)
+        else:
+            raise SeverError
 
-server = loop.run_until_complete(coro)
+        return response
 
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    pass
+    def put(self, name, value, timestamp):
+        pass
 
-server.close()
-loop.run_until_complete(server.wait_closed())
-loop.close()
+    def get(self, name):
+        pass
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    coroutine = loop.create_server(
+        ClientServerProtocol,
+        '127.0.0.1', 8181
+    )
+
+    server = loop.run_until_complete(coroutine)
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+
+    server.close()
+    loop.run_until_complete(server.wait_closed())
+    loop.close()
